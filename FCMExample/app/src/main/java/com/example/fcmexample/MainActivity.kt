437ce519d9b300aa58297812bef289cfb87270e6
DHKim <dhkim0405@naver.com>
2022-02-10 16:03:56 +0900

    FCMExample 특정 디바이스로 푸시 메시지보내기 추가

--- FCMExample/app/src/main/java/com/example/fcmexample/MainActivity.kt
+++ FCMExample/app/src/main/java/com/example/fcmexample/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.fcmexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.fcmexample.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val TAG : String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        binding.btnGetToken.setOnClickListener {
+            getToken()
+        }//setOnClickListener
+    }//onCreate()
+
+    
+    private fun getToken(){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, token.toString())
+            Toast.makeText(baseContext, token.toString(), Toast.LENGTH_SHORT).show()
+        })
+    }//getToken()
 }
